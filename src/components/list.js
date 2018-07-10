0d37789d0552de7ce3d1225cba15f53d4143294d
Tom Wilson <tom@jackhq.com>
2018-07-10 07:07:07 -0400

    removed list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,33 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { map } from 'ramda'
-import { List, ListItem, ListItemText } from '@material-ui/core'
 
-const li = movie => {
-  console.log(movie)
-  return (
-    <ListItem key={movie.imdbId}>
-      <ListItemText>{movie.Title}</ListItemText>
-    </ListItem>
-  )
-}
-
-const Movies = ({ movies }) => <List>{map(li, movies)}</List>
-
-Movies.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      Title: PropTypes.string,
-      Poster: PropTypes.string,
-      Year: PropTypes.string,
-      Type: PropTypes.string,
-      imdbId: PropTypes.string
-    })
-  )
-}
-
-Movies.defaultProps = {
-  movies: []
-}
+const Movies = () => <div>TODO: List Movies</div>
 
 export default Movies
