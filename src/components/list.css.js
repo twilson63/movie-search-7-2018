924080bfc506d036ec56dde1deea2936de57399a
Tom Wilson <tom@jackhq.com>
2018-07-10 11:17:23 -0400

    added list.css

---
+++ src/components/list.css.js
@@ -0,0 +1,14 @@
+export default {
+  card: {
+    display: 'flex'
+  },
+  cardContent: {
+    flex: 'auto'
+  },
+  media: {
+    minHeight: 251,
+    minWidth: 151,
+    padding: 16,
+    margin: 16
+  }
+}
