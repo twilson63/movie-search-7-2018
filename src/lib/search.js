f8125ed4802acb1ae1d95d2658651d990cd9911d
Tom Wilson <tom@jackhq.com>
2018-07-10 07:12:47 -0400

    removed search

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,26 +1,2 @@
-/* global fetch */
-const searchUrl = process.env.URL
-const token = process.env.TOKEN
-
-/**
- * search
- *
- * searches for movies based on criteria
- *
- * @param criteria {string} - search criteria for moview
- * @returns results {array} - array of movies or emty array
- */
-const search = async criteria => {
-  const response = await fetch(`${searchUrl}?q=${encodeURI(criteria)}`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  const data = await response.json()
-  if (data.Response === 'False') {
-    return []
-  }
-  return data.Search
-}
-
+const search = () => Promise.resolve([])
 export default search
