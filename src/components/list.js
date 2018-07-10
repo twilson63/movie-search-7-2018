a22e5825146682233e914c28ba2e53fdb47e8c1f
Tom Wilson <tom@jackhq.com>
2018-07-10 11:15:52 -0400

    updated list answer

--- src/components/list.js
+++ src/components/list.js
@@ -1,5 +1,39 @@
 import React from 'react'
+import { map } from 'ramda'
+import styles from './list.css.js'
+import {
+  withStyles,
+  Card,
+  CardContent,
+  Typography,
+  CardMedia
+} from '@material-ui/core'
 
-const Movies = () => <div>TODO: List Movies</div>
+const createLineItem = classes => movie => {
+  return (
+    <Card className={classes.card}>
+      <CardContent className={classes.cardContent}>
+        <Typography variant="headline">{movie.Title}</Typography>
+        <Typography variant="subheading">Year: {movie.Year}</Typography>
+      </CardContent>
+      <CardMedia
+        className={classes.media}
+        image={movie.Poster}
+        title={movie.Title}
+      />
+    </Card>
+  )
+}
 
-export default Movies
+const Movies = ({ movies, classes }) => {
+  return (
+    <div>
+      <p>Results</p>
+      <div>{map(createLineItem(classes), movies)}</div>
+    </div>
+  )
+}
+
+const higherOrderComponent = withStyles(styles)
+
+export default higherOrderComponent(Movies)
